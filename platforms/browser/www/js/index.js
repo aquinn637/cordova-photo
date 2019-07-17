7f12bbd4e45391e52baca4f257fdd86d7e8598f9
Aubrey Quinn <aquinn@softworks.com>
2019-07-17 11:19:19 +0100

    reproduced issue

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,56 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+    receivedEvent: function(id) {
+
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+
+};
+
+app.initialize();
+
+
+
+function startCamera() {
+        
+    console.log(' start camera ');
+    //configure the cordova-camera
+
+    var options = {
+        targetWidth: 150,
+        targetHeight: 150,
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: false,
+        encodingType: Camera.EncodingType.JPEG,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false,
+        correctOrientation: true
+    };
+
+    navigator.camera.getPicture( successCallback, errorCallback, options );
+
+        function successCallback(imageData) {
+            console.log(imageData);
+        }
+
+        function errorCallback(error) {
+            console.log(error);
+        }
+
+}
